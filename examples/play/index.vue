f518098329d567d6287b7e79fbb4afe7cdfdb918
fengjutian <2640111081@qq.com>
2023-06-11 18:23:15 +0800

    save

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div style="margin: 20px;">
     <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <el-button-my type="primary">主要按钮</el-button-my>
   </div>
 </template>
 
